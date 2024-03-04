c06b1fcbd0533d2aad1f5e7d9861600152e73639
raavikant27 <sravikant302@gmail.com>
2024-03-05 01:25:39 +0530

    first commit

---
+++ src/data.jsx
@@ -0,0 +1,66 @@
+import { nanoid } from "nanoid";
+import { FaHtml5, FaJs, FaReact } from "react-icons/fa";
+
+export const links = [
+  { id: nanoid(), href: "#home", text: "home" },
+  { id: nanoid(), href: "#skills", text: "skills" },
+  { id: nanoid(), href: "#about", text: "about" },
+  { id: nanoid(), href: "#projects", text: "projects" },
+];
+
+export const skills = [
+  {
+    id: nanoid(),
+    title: "HTML&CSS",
+    icon: <FaHtml5 className="h-16 w-16 text-emerald-500" />,
+    text: "Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
+  },
+  {
+    id: nanoid(),
+    title: "Javascript",
+    icon: <FaJs className="h-16 w-16 text-emerald-500" />,
+    text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
+  },
+  {
+    id: nanoid(),
+    title: "React",
+    icon: <FaReact className="h-16 w-16 text-emerald-500" />,
+    text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+  },
+];
+
+export const projects = [
+  {
+    id: nanoid(),
+    img: "https://images.pexels.com/photos/326503/pexels-photo-326503.jpeg?auto=compress&cs=tinysrgb&w=800",
+    url: "https://treevisualiser.vercel.app/",
+    github: "",
+    title: "TreeVisualiser",
+
+    text: "Tree Data Structure is very important specially interms of it’s visualizations.TreeHouse is all about the clean visualization of tree data Structure of many forms say Binary tree.",
+  },
+  {
+    id: nanoid(),
+    img: "https://images.pexels.com/photos/2148222/pexels-photo-2148222.jpeg?auto=compress&cs=tinysrgb&w=800",
+    url: "https://crypto-hunter.netlify.app/",
+    github: "",
+    title: "Crypto-app",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.",
+  },
+  {
+    id: nanoid(),
+    img: "https://images.pexels.com/photos/12883026/pexels-photo-12883026.jpeg?auto=compress&cs=tinysrgb&w=800",
+    url: "https://notesapp-ashy.vercel.app/",
+    github: "",
+    title: "Notes-app",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.",
+  },
+  {
+    id: nanoid(),
+    img: "https://images.pexels.com/photos/12883026/pexels-photo-12883026.jpeg?auto=compress&cs=tinysrgb&w=800",
+    url: "https://random-gif-lemon.vercel.app/",
+    github: "https://github.com/john-smilga",
+    title: "Meme-genarater-app",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.",
+  },
+];
